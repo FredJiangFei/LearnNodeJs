8668ed231dadd2b3bab3e6a230e8aeb0f7a75393
FredJiang <jiangf@shinetechchina.com>
2019-03-14 09:50:57 +0800

    npm

--- node-package-manager/index.js
+++ node-package-manager/index.js
@@ -1,7 +1,9 @@
 const _ = require('underscore');
+const npmFred = require('npm-pub-fred');
 
 // Core module
 // File or folder
 // node-modules
 
 console.log(_.contains([1, 2, 3], 2));
+console.log(npmFred.add(1, 2));
---
+++ npm-pub/index.js
@@ -0,0 +1,7 @@
+module.exports.add = (a, b) => {
+  return a + b;
+};
+
+module.exports.mul = (a, b) => {
+  return a * b;
+};
