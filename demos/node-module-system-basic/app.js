e07fd463f115911cd9eb5902e1d159b76dcd97ee
FredJiang <jiangf@shinetechchina.com>
2019-03-18 13:53:07 +0800

    crypto

--- demos/node-module-system-basic/app.js
+++ demos/node-module-system-basic/app.js
@@ -1,8 +1,15 @@
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const http = require('http');
+// const path = require('path');
+// const os = require('os');
+// const fs = require('fs');
+// const http = require('http');
+const crypto = require('crypto');
 
+const secret = 'abcdefg';
+const hash = crypto
+  .createHmac('sha256', secret)
+  .update('I love cupcakes')
+  .digest('hex');
+console.log(hash);
 // Path
 // console.log(path.parse(__filename));
 
@@ -16,29 +23,29 @@ const http = require('http');
 // });
 
 //Event
-const Logger = require('./logger');
-const logger = new Logger();
-logger.on('messagelog', args => {
-  console.log(`Message log ${args.name}`);
-});
-logger.log('Fred');
+// const Logger = require('./logger');
+// const logger = new Logger();
+// logger.on('messagelog', args => {
+//   console.log(`Message log ${args.name}`);
+// });
+// logger.log('Fred');
 
-//Http
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.write('Hello world');
-  }
+// //Http
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.write('Hello world');
+//   }
 
-  if (req.url === '/api/courses') {
-    res.write(JSON.stringify([1, 2, 3]));
-  }
+//   if (req.url === '/api/courses') {
+//     res.write(JSON.stringify([1, 2, 3]));
+//   }
 
-  res.end();
-});
-server.listen(3000);
+//   res.end();
+// });
+// server.listen(3000);
 
-server.on('connection', socket => {
-  console.log('New connection...');
-});
-server.listen(3000);
-console.log('Listening on port 3000...');
+// server.on('connection', socket => {
+//   console.log('New connection...');
+// });
+// server.listen(3000);
+// console.log('Listening on port 3000...');
---
+++ demos/node-module-system-basic/crypto.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+const secret = 'abcdefg';
+const hash = crypto
+  .createHmac('sha256', secret)
+  .update('I love cupcakes')
+  .digest('hex');
+console.log(hash);
